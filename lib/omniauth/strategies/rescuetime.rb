ebde1a1a0ac5a78949deb81a366a15fa83a740f2
Stefanie Nickerson <smnickerson@gmail.com>
2014-08-01 17:29:10 -0400

    debugging

--- lib/omniauth/strategies/rescuetime.rb
+++ lib/omniauth/strategies/rescuetime.rb
@@ -30,6 +30,7 @@ module OmniAuth
       extra { raw_info }
 
       def raw_info
+        Rails.logger.info @raw_info
         @raw_info# ||= access_token.get("https://api.cloudsdale.org/v2/me.json").parsed
       end
 
