10fe1ca28b2d96bea63fd5fc25ea78736a64738b
Stefanie Nickerson <smnickerson@gmail.com>
2014-08-04 14:25:28 -0400

    test

--- lib/omniauth/strategies/rescuetime.rb
+++ lib/omniauth/strategies/rescuetime.rb
@@ -30,8 +30,7 @@ module OmniAuth
       extra { raw_info }
 
       def raw_info
-        # @raw_info ||= access_token.get("https://api.cloudsdale.org/v2/me.json").parsed
-        @raw_info
+        @raw_info ||= {}
       end
 
     end
